615111d7f582f62d0e3fd4bf261669de27067445
Seatigh <100303493@alumnos.uc3m.es>
2017-03-16 13:13:42 +0100

    V1 R0 C0
    Finished setup. Reorganiced.
    Started tutorial.
    Completed: 1.2.8

---
+++ GettingStarted/tut/tut5.erl
@@ -0,0 +1,24 @@
+%% This is a consolidation of maths, atoms, tuples and list tutorials.
+%% 	- tut.erl
+%% 	- tut1.erl
+%% 	- tut2.erl
+%% 	- tut3.erl
+%% 	- tut4.erl
+
+-module(tut5).
+-export([format_temps/1]).
+
+%% Only this function is exported
+format_temps([]) ->
+	ok;
+format_temps([City | Rest]) ->
+	print_temp(convert_to_celsius(City)),
+	format_temps(Rest).
+
+convert_to_celsius({Name, {c, Temp}}) -> % No conversion needed
+	{Name, {c, Temp}};
+convert_to_celsius({Name, {f, Temp}}) -> % Do the conversion
+	{Name, {c, (Temp -32) * 5 / 9}}.
+
+print_temp({Name, {c, Temp}}) ->
+	io:format("~-15w ~w c~n", [Name, Temp]).
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BitOfNewLang
 Getting started in Erlang and improving new styles of programming.
+
+Documentation: www.erlang.org/docs
+Tutorial doc: erlang.org/download/getting_started-5.4.pdf
