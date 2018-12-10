dc9dc2710f3cb5b771515f137f998cafe26da781
Seatigh <100303493@alumnos.uc3m.es>
2018-12-10 13:15:33 +0100

    Actualización de fase

--- tests/test2.erl
+++ tests/test2.erl
@@ -1,6 +1,8 @@
 -module (test2).
 -export ([loop/1]).
 
+loop(0) ->
+	io:format('No more numbers~n');
 loop(N) ->
-io:format('Hello Number:~w\n', [N]),
-loop(N+1).
+	io:format('Hello Number:~w~n', [N]),
+	loop(N-1).
